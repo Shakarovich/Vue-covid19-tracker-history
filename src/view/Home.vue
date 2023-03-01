d4c1fdeacb78d11131920a89b8cb1870992a8eeb
sigma <89007924+Shakarovich@users.noreply.github.com>
2023-03-01 13:45:43 +0500

    first commit

---
+++ src/view/Home.vue
@@ -0,0 +1,55 @@
+<template>
+  <main v-if="!loading" >
+     <DataTitle :text="title" :dataDate="dataDate"/>
+     <DataBoxes :stats="stats"/>
+     <CountrySelect @get-country="getCountryData" :countries="countries"/>
+
+  </main>
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10 mb-6">
+      Fetching Data
+    </div>
+    <span>Loading...</span>
+  </main>
+</template>
+
+<script>
+import DataTitle from "@/components/DataTitle.vue";
+import DataBoxes from "@/components/DataBoxes.vue";
+import CountrySelect from "@/components/CountrySelect.vue";
+  export default {
+    name: 'Home',
+    components: {
+      DataTitle,
+      DataBoxes,
+      CountrySelect
+    },
+    data() {
+      return {
+        loading : true,
+        title: 'Global',
+        dataDate: '',
+        status: {},
+        countries: [],
+        // loadingImage: require('')
+      }
+    },
+    methods: {
+      async fetchCovidData (){
+        const res = await fetch('https://api.covid19api.com/summary')
+        const data = await res.json()
+        return data
+      },
+      getCountryData(country) {
+        this.stats = country
+      }
+    },
+    async created() {
+      const data = await this.fetchCovidData()
+      this.dataDate = data.Date
+      this.stats = data.Global
+      this.countries = data.countries
+      this.loading = false
+    }
+  }
+</script>
